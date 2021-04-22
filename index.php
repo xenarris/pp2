dc0cf727316a6a232b948896df9d60db67ae3f4d
Jean-Kenneth Antonio <jantonio2@mail.greenriver.edu>
2021-04-22 15:21:30 -0400

    Created and set up index page

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program 2</title>
+    <h1>Pair Program 2</h1>
+    <?php
+    /*
+     * Jean-Kenneth (Jake) Antonio
+     * April 22, 2021
+     * 328/pp2/index.php
+     * Pair Program 2
+     */
+
+    //Turn on error reporting
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+    ?>
+</head>
+<body>
+<?php
+echo "PHP Array Practice";
+
+
+
+
+?>
+
+
+
+</body>
+</html>
+
